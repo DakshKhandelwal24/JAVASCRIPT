9320275f5a302dc0075eeadb5f881e6dfa28fe02
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-16 14:59:55 +0530

    added files

--- 01 lecture/index.js
+++ 01 lecture/index.js
@@ -1,3 +1,9 @@
-let a = 55;
-let b = 20;
-console.log(a+b)
+// let a = 55;
+// let b = 20;
+// console.log(a+b)
+// // console.log(7 == 07)
+// false      => 0
+// true       => 1
+// null       => 0
+// undefined  => NaN
+// " "        =>  0
