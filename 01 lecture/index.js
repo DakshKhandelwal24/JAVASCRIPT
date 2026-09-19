2e209efc7f88b98d29d63e0665a7fbd7853c57ee
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-19 14:54:03 +0530

    added files

--- 01 lecture/index.js
+++ 01 lecture/index.js
@@ -11,5 +11,13 @@
 // let stored_Pin = 4565
 // let recent_Pin = 4565
 // let correct_Pass = stored_Pin === recent_Pin
-// console.log(correct_Pass)
- 
+// // console.log(correct_Pass)
+//  let current_Theme= "light";
+//  let saved_Theme = "dark";
+//  let pin_Equallancy= current_Theme==saved_Theme;
+
+//  console.log(pin_Equallancy)
+// let saved_Language="english";
+// let current_Language="hindi";
+// let language_matches= saved_Language != current_Language;
+// console.log(`is language matches ` ,language_matches )
